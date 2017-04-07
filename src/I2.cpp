40f6997ef0dc5e170205fc003cbc9ab830b015d7
Blaza <radebeka@gmail.com>
2017-04-08 00:59:25 +0200

    Adapt I2 statistic to use |X_k+X_l|

--- src/I2.cpp
+++ src/I2.cpp
@@ -7,21 +7,21 @@ double I2_Cpp(const NumericVector& X) {
     int n = X.size();
     int64_t TS_sum = 0;
     int i,j,k,l;
-    double aXimXj, aXipXj;
+    double aXimXj, aXipXj, aXkpXl;
     for(i = 0; i < n; i++) {
         for(j = 0; j < n; j++) {
             aXimXj = std::abs(X[i] - X[j]);
             aXipXj = std::abs(X[i] + X[j]);
             for(k = 0; j < n; j++) {
                 for(l = 0; l < n; l++) {
-                    TS_sum += aXimXj < X[k]+X[l];
-                    TS_sum -= aXipXj < X[k]+X[l];
+                    aXkpXl = std::abs(X[k]+X[l]);
+                    TS_sum += aXimXj < aXkpXl;
+                    TS_sum -= aXipXj < aXkpXl;
                 }
             }
         }
     }
 
-    Rcout << TS_sum <<std::endl;
     double TS_value = (double)TS_sum / pow(n, 4);
 
     return TS_value;
