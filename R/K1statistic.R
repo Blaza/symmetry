301c063233cc412d610be413dd6fd07ac2939a07
Blaza <radebeka@gmail.com>
2017-04-08 00:14:25 +0200

    Make fast C implementation of K1 statistic default

--- R/K1statistic.R
+++ R/K1statistic.R
@@ -1,30 +1,9 @@
-K1_fun <- function(X, k, t, chunk_size) {
-  n <- length(X)
-
-  Xs <- sort(X)
-
-  index_set <- iterpc::iterpc(n, 2*k)
-  IS_iter <- iterpc::iter_wrapper(index_set, chunk_size)
-
-  chunk_sums <- foreach(comb = IS_iter, .combine = c,
-                        .packages = c("symmetry")) %do% {
-    sum(abs(Xs[comb[ ,k]]) < t) - sum(abs(Xs[comb[ ,k+1]]) < t)
-  }
-
-  abs(sum(chunk_sums)) / choose(n, 2*k)
-}
-
 #' @export
-K1 <- function(X, k, chunk_size=-1L) {
-  n <- length(X)
-  if(chunk_size < 0) chunk_size <- best_chunk_size(n, 2*k)
-
-  fun <- function(t) K1_fun(X, k, t, chunk_size)
-
-  aX <- sort(abs(X))
+K1 <- function(X, k) {
+  saX <- sort(abs(X))
   # potential maximum points
-  pts <- c(rbind(aX, aX + c(diff(aX)/2, 0)))
+  pts <- c(saX[1], c(rbind(saX[-1] - diff(saX)/2, saX[-1])))
 
-  max(sapply(pts,fun))
+  K1_Cpp(X, pts, k)
 }
 
