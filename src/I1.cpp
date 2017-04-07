69547e073ea81ac8d72020388ea1c5a1ed41d2b1
Blaza <radebeka@gmail.com>
2017-04-07 22:07:30 +0200

    remove excess comments from I1.cpp

--- src/I1.cpp
+++ src/I1.cpp
@@ -47,19 +47,4 @@ double I1_Cpp(const NumericVector& X, int k) {
 
     return TS_value;
 }
-  //int n = x.size();
 
-  //NumericVector xs = clone(x);
-  //std::sort(xs.begin(), xs.end());
-
-  //int sum = 0;
-
-  //int j = 0;
-  //double axi;
-  //for(int i = 0; i < n; i++){
-    //axi = std::abs(x[i]);
-    //for(j = 0; j < chunksize; j++){
-      //sum += (std::abs(xs[comb(j, k-1) - 1]) < axi);
-      //sum -= (std::abs(xs[comb(j, k) - 1]) < axi);
-    //}
-  //}
