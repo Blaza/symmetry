ad683ac58e0f78742ff51761284f950277dc1517
Blaza <radebeka@gmail.com>
2017-04-07 17:48:11 +0200

    Remove old implementation of I1C

--- R/I1statistic.R
+++ R/I1statistic.R
@@ -40,20 +40,3 @@ I1C <- function(X, k, chunk_size=-1L) {
   sum(chunk_sums) / (n * choose(n, 2*k))
 }
 
-##' @export
-#I1C <- function(X, k, chunk_size=-1L) {
-  #n <- length(X)
-  #if(chunk_size < 0) chunk_size <- best_chunk_size(n, 2*k)
-
-  #Xs <- sort(X)
-
-  #index_set <- iterpc::iterpc(n, 2*k)
-  #IS_iter <- iterpc::iter_wrapper(index_set, chunk_size)
-
-  #chunk_sums <- foreach(comb = IS_iter, .combine = c,
-                        #.packages = c("symmetry")) %do% {
-    #chunk_sum(abs(Xs[comb[ ,k]]), abs(Xs[comb[ ,k+1]]), abs(X))
-  #}
-
-  #sum(chunk_sums) / (n * choose(n, 2*k))
-#}
--- src/cppFunctions.cpp
+++ src/cppFunctions.cpp
@@ -2,26 +2,6 @@
 #include "combinations.h"
 using namespace Rcpp;
 
-//// [[Rcpp::export]]
-//int chunk_sum(NumericVector kc, NumericVector kc1, NumericVector x){
-  //int chunksize = kc.size();
-  //int n = x.size();
-
-  //int sum = 0;
-  //int ind = 0;
-  //int ind1 = 0;
-  //for(int i = 0; i < n; i++){
-    //for(int j = 0; j < chunksize; j++){
-      //ind = kc[j] < x[i] ? 1 : 0;
-      //ind1 = kc1[j] < x[i] ? 1 : 0;
-
-      //sum += ind - ind1;
-    //}
-  //}
-
-  //return sum;
-//}
-
 // [[Rcpp::export]]
 int chunk_sum(IntegerMatrix comb, NumericVector xs, NumericVector x, int k){
   int chunksize = comb.nrow();
