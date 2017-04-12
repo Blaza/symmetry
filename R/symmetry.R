e2cb59b9b67b693dddf7bc2465c67f1e554eabe2
Blaza <radebeka@gmail.com>
2017-04-13 01:08:09 +0200

    Move tvalues to a new file and document all

--- R/symmetry.R
+++ R/symmetry.R
@@ -1,51 +1,9 @@
+#' symmetry: A package which implements tests for symmetry`
+#' @docType package
+#' @name symmetry
+NULL
+
 #' @useDynLib symmetry
 #' @importFrom Rcpp sourceCpp
 #' @import parallel
 
-
-#' @export
-Tvalues <- function(N, n, dist=list(), TS=list()) {
-  if(!is.list(dist) && is.character(dist))
-    dist <- list(name = dist)
-
-  rdist <- paste0("r", dist$name)
-  distparams <- dist[ which(names(dist) != 'name') ]
-
-  samples <- matrix(do.call(rdist, c(N * n, distparams)), ncol = n)
-
-  if(!is.list(TS) && is.character(TS))
-    TS <- list(name = TS)
-
-  TSparams <- TS[ which(names(TS) != 'name') ]
-  apply(samples, 1, function(x) do.call(TS$name, c(list(x), TSparams)))
-}
-
-#' @export
-parTvalues <- function(N, n, dist=list(), TS=list(), freecores=0) {
-  if(!is.list(dist) && is.character(dist))
-    dist <- list(name = dist)
-
-  rdist <- paste0("r", dist$name)
-  distparams <- dist[ which(names(dist) != 'name') ]
-
-  samples <- matrix(do.call(rdist, c(N * n, distparams)), ncol = n)
-
-  if(!is.list(TS) && is.character(TS))
-    TS <- list(name = TS)
-
-  TSparams <- TS[ which(names(TS) != 'name') ]
-
-  no_cores <- detectCores() - freecores
-  # Initiate cluster
-  cl <- makeCluster(no_cores)
-
-  clusterExport(cl = cl, list("TSparams"), envir = environment())
-  clusterEvalQ(cl, library(symmetry))
-
-  Tvals <- parRapply(cl, samples,
-                     function(x) do.call(TS$name, c(list(x), TSparams)))
-
-  stopCluster(cl)
-
-  Tvals
-}
