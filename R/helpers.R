1e1e01c8b90cb830d55483a30b90f2c4639b7e28
Blaza <radebeka@gmail.com>
2017-04-13 01:33:54 +0200

    document test_power and remove best_chunk_size

--- R/helpers.R
+++ R/helpers.R
@@ -1,8 +1,11 @@
-best_chunk_size <- function(n, k) {
-  nCk <- choose(n, k)
-  ifelse(nCk > 2e5, round(nCk / 5), nCk)
-}
-
+#' Calculate the power of a test
+#'
+#' This function calculates the power of a test given the null and alternative
+#' T values and the significance level.
+#'
+#' @param t0 the vector of null T values
+#' @param t1 the vector of alternative T values
+#' @param alpha the significance level
 #' @export
 test_power <- function(t0, t1, alpha=0.05) {
   1 - ecdf(t1)(quantile(t0, 1-alpha))
