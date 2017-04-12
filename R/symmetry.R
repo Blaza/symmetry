21c075fd787b1354ef455dcd69a084e1c29025e5
Blaza <radebeka@gmail.com>
2017-04-13 01:09:36 +0200

    Fix imports order

--- R/symmetry.R
+++ R/symmetry.R
@@ -1,9 +1,9 @@
+#' @useDynLib symmetry
+#' @importFrom Rcpp sourceCpp
+#' @import parallel
+NULL
+
 #' symmetry: A package which implements tests for symmetry`
 #' @docType package
 #' @name symmetry
 NULL
-
-#' @useDynLib symmetry
-#' @importFrom Rcpp sourceCpp
-#' @import parallel
-
