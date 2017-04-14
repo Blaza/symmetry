04c3fb3b2dcbc33765e3d0150316374cbc700d8e
Blaza <radebeka@gmail.com>
2017-04-14 20:54:51 +0200

    Add K2U tests

---
+++ tests/testthat/test_K2U.R
@@ -0,0 +1,10 @@
+test_that("K2U works for (1, 2)", {
+  expect_equal(K2U(1:2), 1)
+})
+
+test_that("K2U works for rnorm(50)", {
+  set.seed(1)
+  X <- rnorm(50)
+  expect_equal(K2U(X), 0.03265306, tolerance = 1e-5)
+})
+
