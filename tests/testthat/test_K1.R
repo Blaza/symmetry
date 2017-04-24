a41ac0d7c31f1086e55b8320372a4d2b722452af
Blaza <radebeka@gmail.com>
2017-04-24 23:40:51 +0200

    Update K1 tests

--- tests/testthat/test_K1.R
+++ tests/testthat/test_K1.R
@@ -2,6 +2,10 @@ test_that("K1 works for (1, 2, 3) and k=1", {
   expect_equal(K1(1:3, 1), 2/3)
 })
 
+test_that("K1 works for (1, 2, 3, 4) and k=1", {
+  expect_equal(K1(1:4, 1), 2/3)
+})
+
 test_that("K1 works for rnorm(50) and k=1", {
   set.seed(1)
   X <- rnorm(50)
