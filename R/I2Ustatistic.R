16f4a28c3b5617b09b2f8de1f6d7658df53b146a
Blaza <radebeka@gmail.com>
2017-04-24 23:56:20 +0200

    Update docs

--- R/I2Ustatistic.R
+++ R/I2Ustatistic.R
@@ -2,8 +2,8 @@
 #'
 #' @param X the sample for which to calculate the statistic
 #' @return The value of the test statistic given by the formula:
-#'         \deqn{ \frac{1}{n\choose4} \sum_{1\leq i<j<a<b\leqn} I\{|X_i - X_j| <
-#'         X_a+X_b\}- I\{|X_i + X_j| < X_a+X_b\}}
+#'         \deqn{ \frac{1}{{n\choose4}}{\sum_{1\leq i<j<a<b\leq n}
+#'         I\{|X_i - X_j| < X_a+X_b\}- I\{|X_i + X_j| < X_a+X_b\}} }
 #' @examples
 #' set.seed(1)
 #' X <- rnorm(50)
