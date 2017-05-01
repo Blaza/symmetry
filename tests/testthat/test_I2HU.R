33e5e5cb0347bd92bf026ed9c42b56440d095fe4
Blaza <radebeka@gmail.com>
2017-05-01 14:16:38 +0200

    Add tests to I2U and I2HU

--- tests/testthat/test_I2HU.R
+++ tests/testthat/test_I2HU.R
@@ -2,3 +2,9 @@ test_that("I2HU works for (1, 2)", {
   expect_equal(I2HU(1:2), 3/4)
 })
 
+test_that("I2HU works for rnorm(50)", {
+  set.seed(1)
+  X <- rnorm(50)
+  expect_equal(I2HU(X), 0.007902041, tolerance = 1e-8)
+})
+
--- tests/testthat/test_I2U.R
+++ tests/testthat/test_I2U.R
@@ -10,3 +10,9 @@ test_that("I2U works for (-2, -1, 0, 1, 2)", {
   expect_equal(I2U(-2:2), 2/5)
 })
 
+test_that("I2U works for rnorm(50)", {
+  set.seed(1)
+  X <- rnorm(50)
+  expect_equal(I2U(X), 0.005749023, tolerance = 1e-8)
+})
+
