6fca4a004598629778bb7faadc5a500f82c3d7ce
Blaza <radebeka@gmail.com>
2017-05-22 22:53:00 +0200

    Update docs

--- R/HGstatistic.R
+++ R/HGstatistic.R
@@ -5,8 +5,10 @@
 #' @param t the value with which to compare in indicator
 #' @param H whether to calculate H or G
 #' @return The value of the statistics given by the formula:
-#'         \deqn{ \frac{1}{{n\choose{2k+1}}} \sum_{\mathcal{I}_{2k+1}}
-#'         I\{X_{(k+1),X_{i_1},\ldots,X_{i_{2k+1}}} < t }
+#'         \deqn{ H = \frac{1}{{n\choose{2k+1}}} \sum_{\mathcal{I}_{2k+1}}
+#'         I\{-X_{(k+1),X_{i_1},\ldots,X_{i_{2k+1}}} < t\} }
+#'         \deqn{ G = \frac{1}{{n\choose{2k+1}}} \sum_{\mathcal{I}_{2k+1}}
+#'         I\{X_{(k+1),X_{i_1},\ldots,X_{i_{2k+1}}} < t\} }
 #' @examples
 #' set.seed(1)
 #' X <- rnorm(50)
