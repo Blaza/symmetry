a2bceef5991f29be15a70692086037fd95b5fe71
Blaza <radebeka@gmail.com>
2019-01-03 01:55:32 +0100

    Update docs

--- R/L1statistic.R
+++ R/L1statistic.R
@@ -2,10 +2,10 @@
 #'
 #' @param X the sample for which to calculate the statistic
 #' @param a the tuning parameter for the Laplace transform
-#' @return The value of the test statistic given by the formula: \deqn{
-#'   \frac{1}{n^4} \sum_{i,j,k,l=1}^n \frac{1}{a+|X_i - X_j|+|X_k -
-#'   X_l|}-\frac{1}{a+|X_i - X_j|+|X_k + X_l|} - \frac{1}{a+|X_i + X_j|+|X_k -
-#'   X_l|} +\frac{1}{a+|X_i + X_j|+|X_k + X_l|} }
+#' @return The value of the test statistic given by the formula: \deqn{\begin{array}{rl}
+#'   \frac{1}{n^4} \sum_{i,j,k,l=1}^n &\left(\frac{1}{a+|X_i - X_j|+|X_k -
+#'   X_l|}-\frac{1}{a+|X_i - X_j|+|X_k + X_l|}\right. \\ &-\left.\frac{1}{a+|X_i + X_j|+|X_k -
+#'   X_l|} +\frac{1}{a+|X_i + X_j|+|X_k + X_l|}\right)\end{array} }
 #' @examples
 #' set.seed(1)
 #' X <- rnorm(50)
