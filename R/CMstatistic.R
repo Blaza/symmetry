d92974d95a56776e54392057773e18bc6977947d
Blaza <radebeka@gmail.com>
2019-05-12 13:08:54 +0200

    Update docs

--- R/CMstatistic.R
+++ R/CMstatistic.R
@@ -1,12 +1,9 @@
-#' Calculate _ test statistic (see 'Value' for formula)
+#' Calculate Cabilio–Masaro test statistic (see 'Value' for formula)
 #'
 #' @param X the sample for which to calculate the statistic
 #' @param mu the estimate of the location parameter
 #' @return The value of the test statistic given by the formula:
-#'         \deqn{ \frac{1}{n {n\choose{2k}}} \sum_{\mathcal{I}_{2k}}
-#'         \sum_{i_{2k+1}=1}^n I\{|X_{(k),X_{i_1},\ldots,X_{i_{2k}}} - \mu| <
-#'         |X_{i_{2k+1}} - \mu|\}- I\{|X_{(k+1),X_{i_1},\ldots,X_{i_{2k}}} -
-#'         \mu| < |X_{i_{2k+1}} - \mu|\} }
+#'         <to be added>
 #' @examples
 #' set.seed(1)
 #' X <- rnorm(50)
