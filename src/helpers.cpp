4ef0dee07c34dde4aea470370daa6b882e3fc3fe
Blaza <radebeka@gmail.com>
2019-06-04 23:30:31 +0200

    Fix ukn function

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -30,5 +30,5 @@ int count_smaller(const NumericVector& array, double value) {
 
 
 double ukn(int k, int n) {
-  return std::pow((k-1)/(n-1), n-1) - std::pow((n-k)/(n-1), n-1);
+  return std::pow((k)/(n-1), n-1) - std::pow((n-k-1)/(n-1), n-1);
 }
